e3eac4532485afb729bf1c94bb6ff00981af6076
unknown <kurama8787898@gmail.com>
2024-04-09 17:57:20 +0300

    Failed

--- app/src/main/java/ua/edu/lntu/ipz_31_1_2_bortniuk_andrii/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/ipz_31_1_2_bortniuk_andrii/MainActivity.kt
@@ -1,6 +1,9 @@
 package ua.edu.lntu.ipz_31_1_2_bortniuk_andrii
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +14,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+            val userEmail: EditText = findViewById(R.id.user_email)
+            val userPass: EditText = findViewById(R.id.user_pass)
+            val button: Button = findViewById(R.id.buttonin)
+
+        button.setOnClickListener {
+            val email = userEmail.text.toString().trim()
+            val pass = userPass.text.toString().trim()
+
+            if(email == "" || pass == "")
+                Toast.makeText(this, "Помилка", Toast.LENGTH_SHORT).show()
+            else {
+                Toast.makeText(this, "Успішно", Toast.LENGTH_SHORT).show()
+
+            }
+
+
         }
     }
 }
